aea179a1c00d13fe34aa6a9dff14900747747c30
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2023-05-19 16:38:01 +0300

    auth google

---
+++ src/components/Auth.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { auth, provider } from "../firebase-config";
+import { signInWithPopup } from "firebase/auth";
+
+export const Auth = () => {
+
+    const signInWithGoogle = async () => {
+      const result = await signInWithPopup(auth, provider);
+      console.log(result);
+    }
+
+    return (
+        <div className="auth">
+            <p>Sign In With Google To Continue</p>
+            <button onClick={signInWithGoogle}>Sign In With Google</button>
+        </div>
+    );
+}
